5147675fa3307e697f77c25d03ca3cf2d704d5bb
Sachintha Sampath <sachinsampath42@gmail.com>
2024-05-24 13:06:36 +0530

    feat: implement basic monorepo

---
+++ @app/client/tailwind.config.cjs
@@ -0,0 +1,3 @@
+// import from a package
+
+module.exports = require("@demo/tailwind-config/tailwind.config.cjs")
---
+++ packages/tailwind-config/tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "**/*/index.html",
+      "**/*.{js,ts,jsx,tsx,vue}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          "brand-main": "#414066"
+        }
+      },
+    },
+    plugins: [],
+  }
+  
